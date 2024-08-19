1c3d34509a13f0d0da68927ca2e75a3ad40bfb01
Shs0160 <aladinsue@gmail.com>
2024-08-20 04:41:27 +0900

    last

--- src/main/java/code_SCH/project/config/WebConfig.java
+++ src/main/java/code_SCH/project/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")// 모든 origin 허용, 필요에 따라 특정 origin만 허용할 수도 있음
-                .allowedOrigins("https://to-do-list-client-two.vercel.app", "http://192.168.0.108:3000")
+                .allowedOrigins("https://to-do-list-client-two.vercel.app", "http://192.168.0.108:3000", "http://192.168.0.104:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 지정
                 .allowedHeaders("*"); // 모든 헤더 허용
 
