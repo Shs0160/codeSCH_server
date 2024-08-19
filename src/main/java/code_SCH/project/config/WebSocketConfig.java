11e55da38b7ae9c3bac13e3d7ea39817c17f37ea
giyomdabin <dabinjang01@gmail.com>
2024-08-19 21:02:27 +0900

    웹소켓부분 추가

--- src/main/java/code_SCH/project/config/WebSocketConfig.java
+++ src/main/java/code_SCH/project/config/WebSocketConfig.java
@@ -1,2 +1,17 @@
-package code_SCH.project.config;public class WebSocketConfig {
+package code_SCH.project.config;
+
+import code_SCH.project.config.handler.MyWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new MyWebSocketHandler(), "/ws").setAllowedOrigins("*");
+    }
 }
--- src/main/java/code_SCH/project/config/handler/MyWebSocketHandler.java
+++ src/main/java/code_SCH/project/config/handler/MyWebSocketHandler.java
@@ -1,2 +1,22 @@
-package code_SCH.project.config.handler;public class MyWebSocketHandler {
+package code_SCH.project.config.handler;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+// 여러 개의 클라이언트 요청을 실시간으로 처리해야하므로 핸들러 필요
+public class MyWebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+
+        // 클라이언트로부터 메시지 수신
+        String payload = message.getPayload();
+        System.out.println("Received message: " + payload);
+
+        // 모든 클라이언트에 메시지 전송
+        session.sendMessage(new TextMessage("Hello! " + payload));
+    }
 }
